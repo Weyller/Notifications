16ad37764bbffac1074a0845ce653935f0e65cc8
weyller <weyller@gmail.com>
2017-11-30 13:54:19 -0500

    1st commit

---
+++ app/src/main/java/ca/qc/lbpsb/fusion/notifications/Login.java
@@ -0,0 +1,16 @@
+package ca.qc.lbpsb.fusion.notifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    // test
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
